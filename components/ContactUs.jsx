7d926d485e67f41d3b9cb62135fe5339e7055365
Ido Haim <137721224+IdoHaim@users.noreply.github.com>
2025-04-22 22:25:22 +0300

    Update ContactUs.jsx

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -6,20 +6,20 @@ function ContactUs() {
   
   return (
     <div style={{display:'flex', backgroundColor: 'transparent', padding: '20px', borderRadius: '8px', color: 'white' }}  >
-      {/* אזור Contact Us - ממורכז */}
+
       <Container>
         <Row>
           <Col>
             <div className="text-center">
               <h2>Contact</h2>
-              <p>noaarch</p>
-              <p>45b Borochov st.</p>
-              <p>Kiryat Tivon</p>
-              <p>3604638</p>
-              <p>mob : 972-54-4560190</p>
-              <p>tel : 972-73-7319272</p>
-              <p>fax : 153-544-560190</p>
-              <p>email : <a href="mailto:office@noaarch.com">office@noaarch.com</a></p>
+              <p> Name </p>
+              <p> ... </p>
+              <p> ... </p>
+              <p> ... </p>
+              <p> ... </p>
+              <p> ... </p>
+              <p> ... </p>
+              <p>email : <a href="mailto:abcde.com">office@abcde.com</a></p>
 
             </div>
           </Col>
